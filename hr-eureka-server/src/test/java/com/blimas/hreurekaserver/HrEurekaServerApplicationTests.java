6ab2fa04d442bd2adc2c89efa77d3da6ca732c49
brect <b.limas93@gmail.com>
2021-01-02 22:37:57 -0300

    Configure eureka clients

--- hr-eureka-server/src/test/java/com/blimas/hreurekaserver/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/blimas/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -2,6 +2,7 @@ package com.blimas.hreurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
 class HrEurekaServerApplicationTests {
