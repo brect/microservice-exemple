f4dd710c54f9c791335b6887968a90bfc00054db
brect <b.limas93@gmail.com>
2021-01-04 21:43:52 -0300

    actuator for runtime config refresh

--- hr-worker/src/main/java/com/blimas/hrworker/resource/WorkerResource.java
+++ hr-worker/src/main/java/com/blimas/hrworker/resource/WorkerResource.java
@@ -4,7 +4,8 @@ import com.blimas.hrworker.entities.Worker;
 import com.blimas.hrworker.repository.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Value("${test.config}")
+    private String testConfig;
 
     private final WorkerRepository repository;
 
@@ -25,6 +30,12 @@ public class WorkerResource {
     public WorkerResource(WorkerRepository repository, Environment env) {
         this.repository = repository;
         this.env = env;
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
