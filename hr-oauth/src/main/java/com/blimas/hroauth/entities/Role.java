05169e8c9e7464c68f615f6af6aae3b828293774
brect <b.limas93@gmail.com>
2021-01-06 00:03:06 -0300

    adjust project hr-oauth pom and files

--- hr-oauth/src/main/java/com/blimas/hroauth/entities/Role.java
+++ hr-oauth/src/main/java/com/blimas/hroauth/entities/Role.java
@@ -1,17 +1,60 @@
 package com.blimas.hroauth.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
-import java.util.UUID;
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Role {
+	private Long id;
+	private String roleName;
+	
+	public Role() {
+	}
 
-    private UUID id;
-    private String roleName;
+	public Role(Long id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((roleName == null) ? 0 : roleName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (roleName == null) {
+			if (other.roleName != null)
+				return false;
+		} else if (!roleName.equals(other.roleName))
+			return false;
+		return true;
+	}
 }
--- hr-oauth/src/main/java/com/blimas/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/blimas/hroauth/services/UserService.java
@@ -1,7 +1,5 @@
 package com.blimas.hroauth.services;
 
-import com.blimas.hroauth.entities.User;
-import com.blimas.hroauth.feignClients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +8,35 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.blimas.hroauth.entities.User;
+import com.blimas.hroauth.feignclients.UserFeignClient;
+
 @Service
 public class UserService implements UserDetailsService {
 
-    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: " + email);
+		return user;
+	}
 
-    @Autowired
-    private UserFeignClient userFeignClient;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userFeignClient.findByEmail(username).getBody();
-        if (user == null) {
-            logger.error("Email not found: " + username);
-            throw new UsernameNotFoundException("Email not found");
-        }
-        logger.info("Email found: " + username);
-        return user;
-    }
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
+		return user;
+	}
 }
